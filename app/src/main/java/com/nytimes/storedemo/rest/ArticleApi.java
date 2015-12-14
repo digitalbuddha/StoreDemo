fca42e39110d36cbf8686317b488d74ad4681658
Mike Nakhimovich <mnakhimovich@gmail.com>
2015-12-13 19:09:55 -0500

    cleanup

--- app/src/main/java/com/nytimes/storedemo/rest/ArticleApi.java
+++ app/src/main/java/com/nytimes/storedemo/rest/ArticleApi.java
@@ -1,5 +1,11 @@
 package com.nytimes.storedemo.rest;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.net.URL;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -15,6 +21,13 @@ public class ArticleApi {
     }
 
     public Observable<String> getArticles() {
-        return Observable.fromCallable(() -> "fake articles");
+        URL url = Resources.getResource("src/main/resources/articles.json");
+        try {
+            String text = Resources.toString(url, Charsets.UTF_8);
+            return Observable.fromCallable(() -> text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
