e46d44d8eec887b110272a1bac1e6781ea50494a
Mike Nakhimovich <mnakhimovich@gmail.com>
2015-12-19 15:03:06 -0500

    reddit api

--- app/src/main/java/com/nytimes/storedemo/di/AppModule.java
+++ app/src/main/java/com/nytimes/storedemo/di/AppModule.java
@@ -6,11 +6,11 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.nytimes.storedemo.model.GsonAdaptersModel;
 import com.nytimes.storedemo.rest.RedditApi;
+import com.nytimes.storedemo.util.MyApplicationUtils;
 import com.squareup.okhttp.Cache;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -19,6 +19,7 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
 import retrofit.converter.GsonConverter;
@@ -48,23 +49,42 @@ public class AppModule {
 
     @Singleton
     @Provides
-    RedditApi provideRedditApi(Gson gson) {
+    OkHttpClient provideClient() {
         OkHttpClient okHttpClient = new OkHttpClient();
         File cacheDir = new File(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
-        try {
-            okHttpClient.setCache(new Cache(cacheDir, 1024));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        okHttpClient.setCache(new Cache(cacheDir, 1024));
+        return okHttpClient;
+    }
+
+    @Singleton
+    @Provides
+    RequestInterceptor provideInteceptor() {
+        return request -> {
+            request.addHeader("Accept", "application/json;versions=1");
+            if (MyApplicationUtils.isNetworkAvailable(context)) {
+                int maxAge = 60; // read from cache for 1 minute
+                request.addHeader("Cache-Control", "public, max-age=" + maxAge);
+            } else {
+                int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale
+                request.addHeader("Cache-Control",
+                        "public, only-if-cached, max-stale=" + maxStale);
+            }
+        };
+    }
+
+
+    @Singleton
+    @Provides
+    RedditApi provideRedditApi(Gson gson, OkHttpClient client, RequestInterceptor interceptor) {
         // Create a Retrofit RestAdapter for our SodaService interface.
         Executor executor = Executors.newCachedThreadPool();
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setExecutors(executor, executor)
-                .setClient(new OkClient(okHttpClient))
+                .setClient(new OkClient(client))
                 .setEndpoint("http://reddit.com")
                 .setConverter(new GsonConverter(gson))
+                .setRequestInterceptor(interceptor)
                 .build();
         return restAdapter.create(RedditApi.class);
     }
-
 }
---
+++ app/src/main/java/com/nytimes/storedemo/util/MyApplicationUtils.java
@@ -0,0 +1,12 @@
+package com.nytimes.storedemo.util;
+
+import android.app.Application;
+
+/**
+ * Created by 206847 on 12/19/15.
+ */
+public class MyApplicationUtils {
+    public static boolean isNetworkAvailable(Application context) {
+        return false;
+    }
+}
