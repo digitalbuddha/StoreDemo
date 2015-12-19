c7fdbf7d98b2221c797d84ff52a9aefb2dec1f6c
Brian Plummer <brian.r.plummer@gmail.com>
2015-12-19 14:53:25 -0500

    start

--- app/src/main/java/com/nytimes/storedemo/model/Children.java
+++ app/src/main/java/com/nytimes/storedemo/model/Children.java
@@ -9,7 +9,6 @@ import java.util.List;
  * Created by brianplummer on 12/19/15.
  */
 @Value.Immutable
-@Gson.TypeAdapters
 public abstract class Children {
     public abstract PostData data();
 }
--- app/src/main/java/com/nytimes/storedemo/model/PostData.java
+++ app/src/main/java/com/nytimes/storedemo/model/PostData.java
@@ -7,7 +7,6 @@ import org.immutables.value.Value;
  * Created by brianplummer on 12/19/15.
  */
 @Value.Immutable
-@Gson.TypeAdapters
 public abstract class PostData {
     public abstract String title();
     public abstract String thumbnail();
