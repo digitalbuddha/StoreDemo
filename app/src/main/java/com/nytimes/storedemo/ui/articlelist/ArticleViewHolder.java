2945289774b28f751265f5558058af172daeeaa2
Brian Plummer <brian.r.plummer@gmail.com>
2015-12-19 15:32:25 -0500

    picasso, perm fix, thumbnail

--- app/src/main/java/com/nytimes/storedemo/ui/articlelist/ArticleViewHolder.java
+++ app/src/main/java/com/nytimes/storedemo/ui/articlelist/ArticleViewHolder.java
@@ -2,10 +2,12 @@ package com.nytimes.storedemo.ui.articlelist;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nytimes.storedemo.R;
 import com.nytimes.storedemo.model.Children;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by brianplummer on 12/16/15.
@@ -13,13 +15,18 @@ import com.nytimes.storedemo.model.Children;
 public class ArticleViewHolder extends RecyclerView.ViewHolder {
 
     private TextView title;
+    private ImageView thumbnail;
 
     public ArticleViewHolder(View itemView) {
         super(itemView);
         title = (TextView) itemView.findViewById(R.id.title);
+        thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
     }
 
     public void onBind(Children article) {
         title.setText(article.data().title());
+        Picasso.with(itemView.getContext())
+                .load(article.data().thumbnail())
+                .into(thumbnail);
     }
 }
