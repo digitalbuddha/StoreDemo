5cc34ab490b499e61d944d9e1bb5e80106928fc8
Brian Plummer <brian.r.plummer@gmail.com>
2015-12-20 03:24:03 -0500

    experimental

--- app/src/main/java/com/digitalbuddha/storedemo/ui/redditlist/PostViewHolder.java
+++ app/src/main/java/com/digitalbuddha/storedemo/ui/redditlist/PostViewHolder.java
@@ -1,5 +1,11 @@
 package com.digitalbuddha.storedemo.ui.redditlist;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,6 +16,7 @@ import com.digitalbuddha.storedemo.model.Children;
 import com.digitalbuddha.storedemo.model.Image;
 import com.digitalbuddha.storedemo.util.DeviceUtils;
 import com.nytimes.storedemo.R;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import javax.inject.Inject;
@@ -45,12 +52,15 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
         }
 
         maxHeight = (int) (screenHeight * .55f);
-        margin = itemView.getContext().getResources().getDimensionPixelSize(R.dimen.post_horizontal_margin);
+        margin = itemView.getContext().
+                getResources()
+                .getDimensionPixelSize(R.dimen.post_horizontal_margin);
         maxWidth = screenWidth - (2 * margin);
     }
 
     public void onBind(Children article) {
         title.setText(article.data().title());
+        itemView.setBackground(new ColorDrawable(Color.WHITE));
 
         Image image = article.data().preview().images().get(0).source();
         BitmapTransform bitmapTransform = new BitmapTransform(maxWidth, maxHeight, image);
@@ -70,12 +80,61 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
         thumbnail.setMinimumHeight(targetHeight);
         thumbnail.requestLayout();
 
+
         Picasso.with(itemView.getContext())
                 .load(image.url())
                 .transform(bitmapTransform)
                 .resize(targetWidth, targetHeight)
                 .centerInside()
                 .placeholder(R.color.gray80)
-                .into(thumbnail);
+                .into(thumbnail, new Callback()
+                {
+                    @Override
+                    public void onSuccess()
+                    {
+                        handleSuccess();
+                    }
+                    @Override
+                    public void onError()
+                    {itemView.setBackground(new ColorDrawable(Color.WHITE));}
+                });
     }
+
+    private void handleSuccess() {
+
+        new AsyncTask<BitmapDrawable, Integer, Palette>(){
+            @Override
+            protected Palette doInBackground(BitmapDrawable... params)
+            {
+                Bitmap bitmap = params[0].getBitmap();
+                Palette palette = Palette.from(bitmap).generate();
+                return palette;
+            }
+
+            @Override
+            protected void onPostExecute(Palette palette)
+            {
+                super.onPostExecute(palette);
+                Palette.Swatch swatch = palette.getLightVibrantSwatch();
+                if(swatch == null) {
+                    swatch = palette.getVibrantSwatch();
+                } else if(swatch == null) {
+                    swatch = palette.getMutedSwatch();
+                } else if(swatch == null) {
+                    swatch = palette.getDarkVibrantSwatch();
+                } else if(swatch == null) {
+                    swatch = palette.getDarkMutedSwatch();
+                }
+                if(swatch!=null)
+                {
+                    title.setTextColor(swatch.getTitleTextColor());
+                    title.setBackgroundColor(swatch.getRgb());
+                    itemView.setBackground(new ColorDrawable(swatch.getRgb()));
+                } else  {
+                    itemView.setBackground(new ColorDrawable(Color.WHITE));
+                }
+            }
+        }.execute((BitmapDrawable) thumbnail.getDrawable());
+    }
+
 }
